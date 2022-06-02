1faa2ee631811640c33f4052f9bd882f706462ba
Saswath1403 <saswathsingh1403@gmail.com>
2022-06-02 22:29:47 +0530

    NodeJs 2nd Day's Assignment Part -2

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,13 +2,15 @@ const express = require('express');
 const logger = require('../logger/logger')
 const helper = require('../util/helper')
 const validator = require('../validator/formatter')
+const lodash = require('lodash');
 
 const router = express.Router();
 
+
 router.get('/test-me', function (req, res) {
    logger.welcome()
-   helper.Date()
-   helper.Month()
+   helper.printDate()
+   helper.printMonth()
    helper.getBatchInfo()
 console.log (validator.trim.trim())
 console.log (validator.changetoLowerCase.toLocaleLowerCase())
@@ -16,6 +18,27 @@ console.log (validator.changetoUperCase.toLocaleUpperCase())
 
     res.send('My first ever api!')
 });
+
+router.get('/hello', function (req, res) {
+let byChunk = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+console.log(lodash.chunk(byChunk,4))
+
+let byTail  = [1,3,5,7,9,11,13,15,17,19]
+console.log(lodash.tail(byTail))
+
+ let arr1 = [1,2,3,4]
+ let arr2 = [3,4,5,6]
+ let arr3 = [5,6,7,8]
+ let arr4 = [7,8,9,10]
+console.log(lodash.union(arr1,arr2,arr3,arr4));
+
+let byfromPairs = [ ['horror' , 'The Shining'] ,  ['drama' , 'Titanic'] , ['thriller' , 'Shutter Island'] , ['fantasy' , 'Pans Labyrinth'] ]
+
+console.log(lodash.fromPairs(byfromPairs))
+
+    res.send('Hello! My name is Saswath :). ') 
+});
+
 
 module.exports = router;
 // adding this comment for no reason
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,13 +1,16 @@
-const Date = function() {
-    console.log ('01/06/2022')
+const printDate = function() {
+    let currentDate = new Date()
+    console.log (currentDate)
 }
-const Month = function() {
-    console.log ('May')
+const printMonth = function() {
+    let currentDate = new Date()
+    let currentMonth = currentDate.getMonth() + 1
+    console.log ('The current month is : ' + currentMonth)
 }
 const getBatchInfo = function() {
-    console.log ('Radon, W3D1, the topic for today is Nodejs module system')
+    console.log ('Radon, W2D3, the topic for today is Nodejs module system')
 }
 
-module.exports.Date = Date
-module.exports.Month = Month
+module.exports.printDate = printDate
+module.exports.printMonth = printMonth
 module.exports.getBatchInfo = getBatchInfo

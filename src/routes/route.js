7794534340ed392cc453839b02cb27069b20aef7
Saswath1403 <saswathsingh1403@gmail.com>
2022-06-03 18:40:34 +0530

    Assignment and Classwork of NodeJS's 3rd Day

--- src/routes/route.js
+++ src/routes/route.js
@@ -36,6 +36,106 @@ router.get('/candidates/:canidatesName', function(req, res){
     res.send('Done')
 })
 
+router.get('/movies', function (req, res){
+    let arr = ['Avengers', 'Harry Potter', 'The Shawshank Redemption' , 'Pursuit of Happiness' , 'Inception' , 'Intersteller']
 
+    console.log(arr)
+    res.send(arr)
+
+})
+router.get('/movies/:indexNumbers', function (req, res){
+    let printMovies = ['Avengers', 'Harry Potter', 'The Shawshank Redemption' , 'Pursuit of Happiness' , 'Inception' , 'Intersteller']
+
+    res.send(printMovies[req.params.indexNumbers] || "Invalid Index")
+})
+
+router.get('/films', function (req, res){
+    let arr1 =[ {
+        "id": 1,
+        "name": "Avengers"
+       }, {
+        "id": 2,
+        "name": "Harry Potter"
+       }, {
+        "id": 3,
+        "name": "Inception"
+       }, {
+        "id": 4,
+        "name": "Intersteller"
+       }]
+       
+    res.send(arr1)
+   
+})
+
+router.get('/films/filmId', function (req, res){
+    let printFilms =[ {
+        "id": 1,
+        "name": "Avengers"
+       }, {
+        "id": 2,
+        "name": "Harry Potter"
+       }, {
+        "id": 3,
+        "name": "Inception"
+       }, {
+        "id": 4,
+        "name": "Intersteller"
+       }]
+
+       let x = req.params.filmId
+       x = x - 1
+       let flag = 0
+       for (let i=0; i<4; i++){
+           if(i==x){
+               flag = 1
+               break
+           }}
+           if(flag==1){
+               return res.send(printFilms[x])
+           }
+           else{
+               return res.send("no movie exists with this id")
+           }
+        })
+
+          // -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+ app.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let lastDigit= arr.pop()
+    let consecutiveSum= lastDigit * (lastDigit+1) / 2
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+  
+  
+    // -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+  app.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let firstDigit= arr[0]
+    let lastDigit= arr.pop()
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+    let missingNumber= consecutiveSum - total
+   
+    res.send(  { data: missingNumber  }  );
+  });
+ 
+   
 module.exports = router;
 // adding this comment for no reason

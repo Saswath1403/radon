8673ec854af6c4a2501de921fb74ed7326f528e7
Saswath1403 <saswathsingh1403@gmail.com>
2022-06-10 04:23:08 +0530

    MongoDB 4th session Assignment

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,23 +1,53 @@
-const authorModel = require("../models/authorModel")
 const bookModel= require("../models/bookModel")
+const publisherModel = require("../models/publisherModel")
+const authorModel = require("../models/authorModel")
 
 const createBook= async function (req, res) {
     let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
+    let authorID= book.author
+    let publisherID = book.publisher
+    if(authorID) {
+
+        let isValid1 = await authorModel.findById(authorID)
+        if(isValid1) {
+            if(publisherID){
+                let isValid2 = await publisherModel.findById(publisherID)
+                if(isValid2){
+                    let bookNew = await bookModel.create(book)
+                    res.send({data: bookNew})
+                } else res.send(" Publisher ID is not correct")
+            } else res.send(" Please enter Publisher ID")
+
+        }else res.send(" Author ID is not correct")
+    }else   res.send(" Please enter Author ID ")
 }
 
 const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
+    let books = await bookModel.find().populate(['author','publisher']) 
     res.send({data: books})
 }
 
-const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
-    res.send({data: specificBook})
 
-}
+const bookUpdate = async function(req,res){
+    let books = await bookModel.find().populate(['author','publisher'])
+     let finalBooks=[]
+    for(let i=0; i < books.length; i++){
+         if(books[i].publisher.name =="Penguin"|| books[i].publisher.name =="HarperCollins")
+        {
+             let updatedBook=await bookModel.updateOne({name:books[i].name},{$set:{isHardcover:true}})
+             finalBooks.push(updatedBook)
+        }
+        if(books[i].author.rating>3.5){
+             let updatedBook=await bookModel.updateOne({name:books[i].name},{$set:{price:books[i].price + 10}})
+             finalBooks.push(updatedBook)
+             }
+        }
+
+             res.send({msg: finalBooks})
+     }
+
 
 module.exports.createBook= createBook
 module.exports.getBooksData= getBooksData
-module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+module.exports.bookUpdate= bookUpdate
+
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,17 +1,29 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId
 
-const bookSchema = new mongoose.Schema( {
+const NewbookSchema = new mongoose.Schema( {
     name: String,
-    author_id: {
+    
+    author: {
         type: ObjectId,
-        ref: "Author"
+        ref: "NewAuthor"
     },
+   
     price: Number,
-    ratings: Number
 
+    ratings: Number,
+
+    publisher: {
+        type: ObjectId,
+        ref: "NewPublisher"
+    },
+
+    isHardCover: {
+        type: Boolean,
+        default: false
+    }
 
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema)
+module.exports = mongoose.model('NewBook', NewbookSchema)

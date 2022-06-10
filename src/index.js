2a3ac2d9ce15299bc885d5ad8c3dba565b2c650f
Saswath1403 <saswathsingh1403@gmail.com>
2022-06-10 20:35:32 +0530

    Assignment of Middleware Session1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const moment = require('moment');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
@@ -8,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Saswath1403:S%40swath9476@cluster0.fjep0.mongodb.net/Saswath1403-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -16,7 +17,13 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        console.log (" Hi, now you are going to see the current time, IP and route of the user below every time I hit the API!");
+        let userRoute = req.originalUrl;
+        let userIP = req.socket.remoteAddress;
+        let time = moment().format();
+        console.log(time)
+        console.log(userIP)
+        console.log(userRoute)
         next();
   }
   );
